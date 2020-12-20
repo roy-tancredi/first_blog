2b99be81ba6de52d13e15e9e585cee836f3fdd19
Roy Tancredi <tancredi1994@gmail.com>
2020-12-20 19:43:30 +0100

    Home, about, admin and some css

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+posts = [
+    {
+        "author": "Gosiak",
+        "title": "First post",
+        "content": "Some great content",
+        "date_posted": "December 19, 2020"
+    },
+    {
+        "author": "Roy Tancredi",
+        "title": "Second post",
+        "content": "Some great content",
+        "date_posted": "December 20, 2020"
+    }
+
+]
+
+
+def home(request):
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog/home.html", context)
+
+
+def about(request):
+    return render(request, "blog/about.html", {"title": "about"})
+
+
+def base(request):
+    return render(request, "blog/base.html")

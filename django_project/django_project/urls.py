f1ef12c295d7aaf1e48d1b61e77e2cdea8a858fd
Roy Tancredi <tancredi1994@gmail.com>
2020-12-29 13:01:44 +0100

    Blog app with user registration and password reset option.

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -17,7 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
 from users import views as users_views
 
 urlpatterns = [
@@ -27,6 +27,14 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', users_views.profile, name='profile'),
     path('register/', users_views.register, name='register'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='users/password_reset.html'), name='password_reset'),
+    path('password-reset-confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(
+        template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complite/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='users/password_reset_done.html'), name='password_reset_done'),
 ]
 
 
